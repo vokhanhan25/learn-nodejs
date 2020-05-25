09803101562c4189c5edd9c5256c22555ff29345
An Vo <vokhanhan25@gmail.com>
2020-05-25 21:12:12 +0700

    UPDATE static files

--- 1-hello/index.js
+++ 1-hello/index.js
@@ -11,7 +11,10 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => res.render('index', {
     name: 'An'
