320924f8959378787bf9b716b5609c5fdca3e584
An Vo <vokhanhan25@gmail.com>
2020-05-26 22:08:46 +0700

    ADD signed cookies

--- 1-hello/index.js
+++ 1-hello/index.js
@@ -14,7 +14,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser('123123dsadasdas'));
 
 
 app.use(express.static('public'));
