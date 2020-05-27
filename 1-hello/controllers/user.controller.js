e8c59a7d44d43f3eb69aba440dc04177c51d9d0a
An Vo <vokhanhan25@gmail.com>
2020-05-27 15:22:49 +0700

    Upload Avatar

--- 1-hello/controllers/user.controller.js
+++ 1-hello/controllers/user.controller.js
@@ -31,6 +31,8 @@ module.exports.get = function(req, res) {
 
 module.exports.postCreate = function(req, res) {
     req.body.id = shortid.generate();
+    console.log(req.file.path);
+    req.body.avatar = req.file.path.split('\\').slice(1).join('/');
 
     db.get('users').push(req.body).write();
     res.redirect('/users');
